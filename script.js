501be4f3a438200f1e6c0284ec01b830f53ac781
brenors <breno.sedrez@gmail.com>
2023-03-23 18:51:37 -0300

    adicionado html, css e js do layout do sidebar

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    toggle.addEventListener("click", ()=>{
+        body.classList.toggle("close");
+    });
+
+    modeSwitch.addEventListener("click", ()=>{
+        body.classList.toggle("dark");
+    });
