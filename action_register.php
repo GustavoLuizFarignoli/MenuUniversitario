3fccd68ea3992d91790bd7daa3646b9dad786c23
Gustavo Luiz Farignoli <gustavolf.educ@gmail.com>
2023-05-10 17:45:32 -0300

    v5.15.7.10  Adequação do registro e do login nas histórias de usuário

--- action_register.php
+++ action_register.php
@@ -9,18 +9,28 @@
 
     if ($tipo == 1){
 
+        $sql = "SELECT Nome FROM usuario WHERE E_mail = '$email'";
+        $result = $conn->query($sql);
 
-        $sql = "INSERT INTO usuario VALUES('$nome','$email', '$hash', null, '$tipo')";
+        if ($result->num_rows > 0) {
+            $message = "Este E-mail já foi registrado";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            echo "<script type='text/javascript'>window.location.href = 'login.php';</script>";
+        } else {
+            $sql2 = "INSERT INTO usuario VALUES('$nome','$email', '$hash', null, '$tipo')";
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Insert Succeed!!!";
-            header("Location: index.php");
-            session_start();
-            $_SESSION["user"] = $email;
-        }
-        else {
-            echo ("alert('Insert fail!!!')");
-            header("Location: login.php");
+            if ($conn->query($sql2) === TRUE) {
+                session_start();
+                $_SESSION["user"] = $email;
+                $message = "Registro Bem-Sucedido";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+                echo "<script type='text/javascript'>window.location.href = 'index.php';</script>";
+            }
+            else {
+                $message = "Não Foi Possível Finalizar o cadastro";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+                echo "<script type='text/javascript'>window.location.href = 'index.php';</script>";
+            }
         }
     }
 
--- connection.php
+++ connection.php
@@ -1,12 +1,15 @@
 <?php
     $servername = "127.0.0.1";
     $username = "root"; #"desenvolvedor";
-    $password = "@BES123";    #"Bes@2022#";
+    $password = "@BES123";#"Bes@2022#";
     $dbname = "mu";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Falha na conexão:" . $conn->connect_error);
+    try{
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    } catch(Exception $e){
+        $message = "Não foi possível estabelecer conexão com o servidor, tente novamente mais tarde";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+        echo "<script type='text/javascript'>window.location.href = 'index.php';</script>";
     }
 
     /*echo "Conexão efetuada com sucesso!!!";*/
