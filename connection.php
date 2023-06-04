dad7db164aaa254b1f0c6f0eaa2ef1bfde9285f1
Aquahz <gabrielmolec@outlook.com>
2023-06-04 11:33:26 -0300

    Adicionada a apresentação à Documentação

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
-    /* Molec 
+    /* Molec*/ 
     $servername = "localhost:3306";
     $username = "root";
     $password = "";
-    $dbname = "mu";*/ 
+    $dbname = "mu";
 
-    /*Vittorio - Connection*/ 
+    /*Vittorio - Connection
     $servername = "localhost:3307";
     $username = "desenvolvedor";
     $password = "Bes@2022#";
-    $dbname = "mu";
+    $dbname = "mu";*/ 
     
     /* Gustavo - Connection
     $servername = "localhost";
